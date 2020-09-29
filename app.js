b3b70650b51c1aa299774104dc67b734e496825f
NP <navdeep.puri@gmail.com>
2020-09-29 15:36:42 -0400

    commented console.log

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 /***
@@ -125,14 +127,16 @@ async function getUserInput(question) {
 
 async function getInput() {
     try {
+        console.log(" ");
         console.log("Please build your team");
+        console.log(" ");
 
         let response = await getUserInput(questionsManager);
         const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
         employees.push(manager);
 
         let teamMember = await getUserInput(nextEmployeeQuestion);
-        console.log(teamMember);
+        // console.log(teamMember);
 
         while (teamMember.teamMemberType === "Engineer" || teamMember.teamMemberType === "Intern") {
 
@@ -142,13 +146,10 @@ async function getInput() {
                 employees.push(engineer);
             }
             else if (teamMember.teamMemberType === "Intern") {
-
                 response = await getUserInput(questionsIntern);
                 let intern = new Intern(response.name, response.id, response.email, response.school);
                 employees.push(intern);
-
             }
-
 
             teamMember = await getUserInput(nextEmployeeQuestion);
         }
@@ -157,29 +158,22 @@ async function getInput() {
         const  html = render(employees);  
 
         // create the output folder if it does not exist
-        const outputPath = path.resolve(__dirname,"output/");
-        console.log("path: " + outputPath);
-        if(!fs.existsSync(outputPath))
+        if(!fs.existsSync(OUTPUT_DIR))
         {
-           
-            fs.mkdirSync(outputPath, err =>{
-                if(err)
-                throw err;
-            });
-        }
-        else
-        {
-            console.log("directory does exit");
+
+            // console.log("output path checking");   
+            // Using fs.mkdirSync() method  to create the directory             
+            fs.mkdirSync(OUTPUT_DIR);
         }
 
-       
-        fs.writeFile(path.resolve(__dirname,"output/team.html"), html, function(err){
-             if(err)
-               throw err;
-        });
+        // console.log("output path " + outputPath);
 
+        // write html to the output/team.html
+        await writeFileAsync(outputPath, html,"utf8");
 
-
+        console.log("\n*********************************************************");
+        console.log("          Sucessfully Generated team.html                ");
+        console.log("**********************************************************");
 
     }
     catch (err) {
@@ -200,22 +194,4 @@ getInput();
 
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
